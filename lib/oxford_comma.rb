64728f935dd152b34927e97a9b64002a78757ea6
Jose Elera <jelera@gmail.com>
2019-03-22 22:50:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,27 +1,16 @@
 def oxford_comma(arr)
-  # if arr.size == 1
-  #   arr.first
-  # elsif arr.size == 2
-  #   arr.join(" and ")
-  # elsif arr.size >= 3
-  #
-  #
-  # end
-
-  case arr.size
-  when 1
-    arr.first
-  when 2
-    arr.join(" and ")
-  when 3
-    output = ""
-    arr.each_with_index do |word, i|
-      if i < arr.length - 1
-        output += "#{word}, "
-      else
-        output += "and #{word}"
-      end
+  phrase = ""
+  arr.each_with_index do | word, i |
+    if arr.length == 1
+      phrase += arr[0]
+    elsif arr.length == 2
+      phrase = arr.join(" and ")
+    elsif i < arr.length - 1
+      phrase += "#{word}, "
+    else
+      phrase += "and #{word}"
     end
   end
+  phrase
 
 end
