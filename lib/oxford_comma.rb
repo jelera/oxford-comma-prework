cc8934d79d8c425460ec4324a894ee3273b10366
Jose Elera <jelera@gmail.com>
2019-03-22 22:36:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+def oxford_comma(arr)
+
+  case arr.size
+  when 1
+    arr.first
+  when 2
+    arr.join(" and ")
+  when 3
+    output = ""
+    arr.each_with_index do |word, i|
+      if i == arr.length - 1
+        output << "and #{word}"
+      else
+        output << "#{word}, "
+      end
+    end
+  end
 
 end
