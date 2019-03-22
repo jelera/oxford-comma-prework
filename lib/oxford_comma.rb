250bea82f5a9f87b62ea88f4b6276e7e7468a57f
Jose Elera <jelera@gmail.com>
2019-03-22 22:50:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,20 @@
 def oxford_comma(arr)
   phrase = ""
+
   arr.each_with_index do | word, i |
+
     if arr.length == 1
       phrase += arr.first
     elsif arr.length == 2
       phrase = arr.join(" and ")
     elsif i < arr.length - 1
-      phrase += "#{word}, "
+      phrase << "#{word}, "
     else
       phrase += "and #{word}"
     end
+
   end
+
   phrase
 
 end
